81885ac435eadeaaa3ac00ddbbde3ba446b0f355
Riley <rileyjohnson99@hotmail.com>
2019-01-08 10:51:00 -0600

    Framework laid out for extending API for outside usage

---
+++ keymanager/utilities/utilities.go
@@ -0,0 +1,11 @@
+package utilities
+
+import "context"
+
+func GetKeyValue(key string) (string, error) {
+	return GetKeyValueWithContext(context.)
+}
+
+func GetKeyValueWithContext(ctx *context.Context, key string) (string, error) {
+
+}
